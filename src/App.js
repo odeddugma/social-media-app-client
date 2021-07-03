f1c8f3ce45c0dffa0d78ac67d9e2803605082655
Oded Dugma <odeddugma@gmail.com>
2021-07-03 14:04:11 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Our App</h1>
-    </div>
-  );
+/* Components */
+import Navbar from "./components/Navbar.jsx";
+
+/* Pages */
+import home from "./pages/home.jsx";
+import login from "./pages/login.jsx";
+import signup from "./pages/signup.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route path="/login" component={login} />
+              <Route path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
