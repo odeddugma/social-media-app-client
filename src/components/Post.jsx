27d1c468dd2a4c2c8cee99eae6fa6e52faf35846
Oded Dugma <odeddugma@gmail.com>
2021-07-06 09:27:47 +0300

    update

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // MUI stuff
 import Card from "@material-ui/core/Card";
@@ -24,6 +26,7 @@ const styles = {
 
 class Post extends Component {
 	render() {
+		dayjs.extend(relativeTime);
 		const {
 			classes,
 			post: {
@@ -53,7 +56,7 @@ class Post extends Component {
 						{userHandle}
 					</Typography>
 					<Typography variant="body2" color="textSecondary">
-						{createdAt}
+						{dayjs(createdAt).fromNow()}
 					</Typography>
 					<Typography variant="body1">{body}</Typography>
 				</CardContent>
