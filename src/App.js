5a74a682a9dc418d60fbb489be813fff4dc08594
Oded Dugma <odeddugma@gmail.com>
2021-07-09 20:14:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const theme = createMuiTheme(themeFile);
 
 let authenticated; 
 const token = localStorage.FBIdToken;
-if(token){
+/* if(token){
   const decodedToken = jwtDecode(token);
   console.log(decodedToken);
   if (decodedToken.exp*1000<Date.now()){
@@ -26,7 +26,7 @@ if(token){
   }else{
     authenticated = true;
   }
-}
+} */
 
 class App extends Component {
   render() {
